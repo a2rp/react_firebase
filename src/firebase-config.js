f23c9e0af6ffe6708d22bfa33b549a817570cc07
Ashish Ranjan <ash.ranjan09@gmail.com>
2022-06-05 01:02:55 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,12 +6,12 @@ import { getFirestore } from "@firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnRkv3UQ9MNXrMrUnXkrTO6IrkhpjTj0c",
-  authDomain: "fir-tutorial-72677.firebaseapp.com",
-  projectId: "fir-tutorial-72677",
-  storageBucket: "fir-tutorial-72677.appspot.com",
-  messagingSenderId: "215264967012",
-  appId: "1:215264967012:web:46491d8e237afc0f652271"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 // Initialize Firebase
